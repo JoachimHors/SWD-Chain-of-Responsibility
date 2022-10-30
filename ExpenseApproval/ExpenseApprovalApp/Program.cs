60d223fb501c73158312ee9767dac0325a7a9972
JoachimHors <jh.hors@gmail.com>
2022-10-30 18:42:24 +0100

    Finished first iteration
    - Client created
    - Example console app created

---
+++ ExpenseApproval/ExpenseApprovalApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExpenseApprovalLib;
+using ExpenseApprovalLib.ConcreteExpenseHandlers;
+
+var jimHalpert = new Sales();
+var dwightSchrute = new AssistantRegionalManager();
+var michaelScott = new RegionalManager();
+var janLevinson = new VicePresident();
+var davidWallace = new CEO();
+
+var expense = new Expense("A lot of paper", 1000);
+var client = new Client();
+
+jimHalpert.SetNext(dwightSchrute);
+dwightSchrute.SetNext(michaelScott);
+michaelScott.SetNext(janLevinson);
+janLevinson.SetNext(davidWallace);
+
+client.SubmitExpense(jimHalpert, expense);
+
