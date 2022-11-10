5a98bba5120e4370a2fb3207a47d0ac3fe45480a
Nivethan1999 <m-iquaz@hotmail.com>
2022-11-10 08:45:13 +0100

    Updated program

--- ExpenseApproval/ExpenseApprovalApp/Program.cs
+++ ExpenseApproval/ExpenseApprovalApp/Program.cs
@@ -7,7 +7,13 @@ var michaelScott = new RegionalManager();
 var janLevinson = new VicePresident();
 var davidWallace = new CEO();
 
+
 var expense = new Expense("A lot of paper", 1000);
+var expense1 = new Expense("A lot of paper", 5000);
+var expense2 = new Expense("A lot of paper", 5001);
+var expense3 = new Expense("A lot of paper", 50000);
+var expense4 = new Expense("A lot of paper", 100000);
+var expense5 = new Expense("A lot of paper", 100001);
 var client = new Client();
 
 jimHalpert.SetNext(dwightSchrute);
@@ -16,4 +22,9 @@ michaelScott.SetNext(janLevinson);
 janLevinson.SetNext(davidWallace);
 
 client.SubmitExpense(jimHalpert, expense);
+client.SubmitExpense(jimHalpert, expense1);
+client.SubmitExpense(jimHalpert, expense2);
+client.SubmitExpense(jimHalpert, expense3);
+client.SubmitExpense(jimHalpert, expense4);
+client.SubmitExpense(jimHalpert, expense5);
 
